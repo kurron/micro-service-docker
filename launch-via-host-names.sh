72f3cf258fc721b786a223a966f882b93a00f840
Ron Kurr <kurr@kurron.org>
2014-10-25 02:50:26 +0000

    Default to interactive start up.

--- launch-via-host-names.sh
+++ launch-via-host-names.sh
@@ -7,5 +7,5 @@ docker run --detach --hostname="mongodb" --name="mongodb" --memory="256m" --volu
 docker run --detach --hostname="rabbitmq" --name="rabbitmq" --memory="256m" --volume /tmp/rabbitmq/log:/data/log --volume /tmp/rabbitmq/mnesia:/data/mnesia kurron/rabbitmq-docker-container:latest
 
 # launch the application, map its port to localhost:8080 and have Docker take care of mapping the MongoDB and RabbitMQ ports to this container
-docker run --detach --hostname="micro-service" --name="micro-service" --memory="256m" --publish 8080:8080 --link mongodb:mongodb --link rabbitmq:rabbitmq  kurron/micro-service-docker:latest
-#docker run --interactive --tty --hostname="micro-service" --name="micro-service" --memory="256m" --publish 8080:8080 --link mongodb:mongodb --link rabbitmq:rabbitmq  kurron/micro-service-docker:latest /bin/bash
+#docker run --detach --hostname="micro-service" --name="micro-service" --memory="256m" --publish 8080:8080 --link mongodb:mongodb --link rabbitmq:rabbitmq  kurron/micro-service-docker:latest
+docker run --interactive --tty --hostname="micro-service" --name="micro-service" --memory="256m" --publish 8080:8080 --link mongodb:mongodb --link rabbitmq:rabbitmq  kurron/micro-service-docker:latest /bin/bash
