6ce959fddb6cbadcce92d41187defaaccb08d13a
Ron Kurr <kurr@kurron.org>
2014-10-25 20:41:40 -0400

    Fixed the launch script

--- launch-via-command-line-arguments.sh
+++ launch-via-command-line-arguments.sh
@@ -7,6 +7,6 @@ docker run --detach --net="bridge" --publish 27017:27017 --hostname="mongodb" --
 docker run --detach  --net="bridge" --publish 5672:5672 --publish 15672:15672 --hostname="rabbitmq" --name="rabbitmq" --memory="256m" --volume /tmp/rabbitmq/log:/data/log --volume /tmp/rabbitmq/mnesia:/data/mnesia kurron/rabbitmq-docker-container:latest
 
 # launch the application, map its port to localhost:8080 and have Docker pass the environment to the application
-docker run --rm --detach --hostname="micro-service" --name="micro-service" --memory="256m" --publish 8080:8080 kurron/micro-service-docker:latest /bin/bash --spring.data.mongodb.host="10.0.2.15" --spring.rabbitmq.host="10.0.2.15"  --info.app.description="command-line" /bin/bash
+#docker run --rm --detach --hostname="micro-service" --name="micro-service" --memory="256m" --publish 8080:8080 kurron/micro-service-docker:latest /bin/bash --spring.data.mongodb.host="10.0.2.15" --spring.rabbitmq.host="10.0.2.15"  --info.app.description="command-line" /bin/bash
 
-docker run --rm --interactive --tty --hostname="micro-service" --name="micro-service" --memory="256m" --publish 8080:8080 kurron/micro-service-docker:latest /bin/bash --spring.data.mongodb.host="10.0.2.15" --spring.rabbitmq.host="10.0.2.15"  --info.app.description="command-line" /bin/bash
+docker run --rm --interactive --tty --hostname="micro-service" --name="micro-service" --memory="256m" --publish 8080:8080 kurron/micro-service-docker:latest /bin/bash --spring.data.mongodb.host="192.168.1.233" --spring.rabbitmq.host="192.168.1.233"  --info.app.description="command-line" /bin/bash
